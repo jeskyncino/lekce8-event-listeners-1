336894f95c54b46bce16c0569926b6692f54ce14
karolyim <matej.karolyi@gmail.com>
2021-04-27 18:42:25 +0200

    [FEATURE] data attributes demo

---
+++ ovoce.js
@@ -0,0 +1,7 @@
+let prvniOvoce = document.querySelector('#muj-div').dataset.ovoce;
+let prvniOvoceJinak = document.querySelector('#muj-div').getAttribute('data-ovoce');
+console.log(prvniOvoce);
+console.log(prvniOvoceJinak);
+
+let dataUrl = document.querySelector('body').dataset.dataUrl;
+console.log(dataUrl);
