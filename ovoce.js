314f475355f44f1e43f9f51b90b8a4190217fbf8
karolyim <matej.karolyi@gmail.com>
2021-04-27 19:42:48 +0200

    [ENHANCEMENT] fruit console log from data attributes changed

--- ovoce.js
+++ ovoce.js
@@ -1,7 +1,8 @@
+//Data Attributes
 let prvniOvoce = document.querySelector('#muj-div').dataset.ovoce;
 let prvniOvoceJinak = document.querySelector('#muj-div').getAttribute('data-ovoce');
 console.log(prvniOvoce);
 console.log(prvniOvoceJinak);
 
-let dataUrl = document.querySelector('body').dataset.dataUrl;
-console.log(dataUrl);
+let druheOvoce = document.querySelector('#muj-druhy-div').dataset.ovoce;
+console.log(druheOvoce);
